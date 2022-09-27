03950b9d798b95fba6e3efc800ae701c22df04cc
DarkReaper231 <blackcreepercz012345@gmail.com>
2022-09-27 03:55:25 +0200

    update

--- maven-scripts/maven-unix.sh
+++ maven-scripts/maven-unix.sh
@@ -3,9 +3,9 @@ printf "Enter baritone version (Example: 1.5.3-664-g1a60d080): "
 read version
 printf "Enter minecraft client version (Example: 1.19.2): "
 read client_version
-mvn install:install-file -Dfile=baritone-fabric-api-$version.jar -DgroupId=cabaletta -DartifactId=baritone-api-fabric -Dversion=$client_version -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+mvn install:install-file -Dfile=baritone-fabric-api-$version.jar -DgroupId=cabaletta -DartifactId=baritone-fabric-api -Dversion=$client_version -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
 printf "Successfully added baritone-fabric-api-$version to maven"
-mvn install:install-file -Dfile=baritone-fabric-standalone-$version.jar -DgroupId=cabaletta -DartifactId=baritone-standalone-fabric -Dversion=$client_version -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+mvn install:install-file -Dfile=baritone-fabric-standalone-$version.jar -DgroupId=cabaletta -DartifactId=baritone-fabric-standalone -Dversion=$client_version -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
 printf "Successfully added itone-fabric-standalone-$version to maven"
-mvn install:install-file -Dfile=baritone-fabric-unoptimized-$version.jar -DgroupId=cabaletta -DartifactId=baritone-unoptimized-fabric -Dversion=$client_version -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+mvn install:install-file -Dfile=baritone-fabric-unoptimized-$version.jar -DgroupId=cabaletta -DartifactId=baritone-fabric-unoptimized -Dversion=$client_version -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
 printf "Successfully added baritone-fabric-unoptimized-$version to maven"
--- maven-scripts/maven-windows.bat
+++ maven-scripts/maven-windows.bat
@@ -1,10 +1,10 @@
 @echo off
 SET /P version="Enter baritone version (Example: 1.5.3-664-g1a60d080): "
 SET /P client_version="Enter minecraft client version (Example: 1.19.2): "
-call mvn install:install-file -Dfile=baritone-api-fabric-%version%.jar -DgroupId=cabaletta -DartifactId=baritone-api-fabric -Dversion=%client_version% -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+call mvn install:install-file -Dfile=baritone-api-fabric-%version%.jar -DgroupId=cabaletta -DartifactId=baritone-fabric-api -Dversion=%client_version% -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
 echo Successfully added baritone-fabric-api-%version% to maven
-call mvn install:install-file -Dfile=baritone-standalone-fabric-%version%.jar -DgroupId=cabaletta -DartifactId=baritone-standalone-fabric -Dversion=%client_version% -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+call mvn install:install-file -Dfile=baritone-standalone-fabric-%version%.jar -DgroupId=cabaletta -DartifactId=baritone-fabric-standalone -Dversion=%client_version% -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
 echo Successfully added baritone-fabric-standalone-%version% to maven
-call mvn install:install-file -Dfile=baritone-unoptimized-fabric-%version%.jar -DgroupId=cabaletta -DartifactId=baritone-unoptimized-fabric -Dversion=%client_version% -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+call mvn install:install-file -Dfile=baritone-unoptimized-fabric-%version%.jar -DgroupId=cabaletta -DartifactId=baritone-fabric-unoptimized -Dversion=%client_version% -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
 echo Successfully added baritone-fabric-unoptimized-%version% to maven
 pause
